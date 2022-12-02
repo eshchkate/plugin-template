7e69883a1b7d76887a3dcb1d0a9ffcae7536649b
GitHub Action <action@github.com>
2022-12-02 17:25:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/eshchkate/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/eshchkate/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.eshchkate.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.eshchkate.plugintemplate.MyBundle
 
 class MyApplicationService {
 
